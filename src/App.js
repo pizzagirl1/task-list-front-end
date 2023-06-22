15984e2e8a8e06129a145347d0c4e18bc3642706
nadia <reach.nadia.saria@gmail.com>
2023-06-22 15:46:01 -0700

    adds patch helper function to call API

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import TaskList from './components/TaskList.js';
 import './App.css';
 import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
 const taskURL = 'https://task-list-api-c17.onrender.com/';
 
@@ -57,13 +57,27 @@ const App = () => {
         return task;
       }
     });
+    patchTaskAPI(updatedTask);
     setTaskData(tasks);
+  };
+
+  const patchTaskAPI = (updatedTask) => {
+    let patchPath = updatedTask.isComplete ? 'mark_incomplete' : 'mark_complete';
+    axios.patch(`${taskURL}/${updatedTask.id}/${patchPath}`)
+    .catch((e) => console.log('error in patch!', e));
   };
 
   const deleteTask = (id) => {
     setTaskData(tasks => {
       return tasks.filter(task => task.id !== id);
     });
+    deleteTaskFromAPI(id);
+  };
+
+  const deleteTaskFromAPI = (id) => {
+    axios.delete(`${taskURL}/${id}`)
+    .then((response) => console.log('deleted!', response.data))
+    .catch((e) => console.log('error!', e));
   };
 
   return (
